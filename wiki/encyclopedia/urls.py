ab0935ea9a718ccc9e6f40b1e2dd8200374351ae
Emad Eldin Gasser <omda.g@outlook.com>
2022-01-06 13:28:07 +0300

    Emad Change 1

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from . import views
+from . import views, util
+
+
 
 urlpatterns = [
-    path("", views.index, name="index")
+
+    path("", views.index, name="index"),
+    path("<str:entry>", views.entry_page, name="EntryPage"),
+    path("/New", views.new_page, name="NewPage")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from markdown2 import Markdown, markdown
 from . import util
 
 
@@ -7,4 +7,17 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def entry_page(request, entry):
+    body = util.get_entry(entry)
+    markdowner = Markdown()
+
+    return render(request, "encyclopedia/EntryPage.html", {
+        "body": markdowner.convert(body),
+        "title": entry,
+        })
+
+
+def new_page(request):
+    return render(request, "encyclopedia/New.html")
 
